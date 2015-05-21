a2ae337ffef0efea72141cebb1f9a603387c5615
Joulies <Joulies@Joul>
2015-05-21 16:09:52 +0300

    q

--- src/gameState/GameState.java
+++ src/gameState/GameState.java
@@ -1,6 +1,11 @@
 package gameState;
 
+import instances.Instances;
+import controller.Controller;
+
 public class GameState {
+
+	protected Controller controller = Instances.getControllerInstance();
 
 	public void handleGameStateChange() {
 
